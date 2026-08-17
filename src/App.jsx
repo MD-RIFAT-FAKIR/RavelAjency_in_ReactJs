70f5549bda5900e6db547ff3d94b6f5164bd79d0
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2026-08-18 02:43:18 +0600

    Created all section and link in App components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Header from './sections/Header'
+import About from './sections/About'
+import Hero from './sections/Hero'
+import Serveces from './sections/Serveces'
+import CatOne from './sections/CatOne'
+import Packages from './sections/Packages'
+import Features from './sections/Features'
+import Footer from './sections/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Serveces />
+      <CatOne />
+      <Packages />
+      <Features />
+      <Footer />
+    </>
   )
 }
 
---
+++ src/sections/Serveces.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Serveces = () => {
+  return (
+    <div>Serveces</div>
+  )
+}
+
+export default Serveces
